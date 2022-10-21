238a5eae7fcecb35e08fa1581cb2d4bc6f235fab
Abdul Rahman <Abdul.Rahman@eton-solutions.com>
2022-10-21 18:14:09 +0530

    Angular JS post and get request to local api code

--- AngularJS_with_apis/myController.js
+++ AngularJS_with_apis/myController.js
@@ -8,12 +8,38 @@
         var productR = function (data) {
             $scope.products = data;
         };
-
-
-        var onError = function (reason) {
-            $scope.error = "Could not fetch the data.";
+        var Postproduct = function (data) {
+            $scope.products = data;
         };
-        $scope.products = product.getProduts().then(productR, onError);
+
+
+        var onError = function (e) {
+            $scope.error = e;
+        };
+
+        var handleGet = () => {
+            product.getProduts().then(productR, onError);
+        }
+
+        var handlePost = (data) => {
+
+            product.postProduts(data).then(Postproduct, onError);
+            productR();
+
+        }
+
+        $scope.get = () => {
+            handleGet();
+        }
+
+
+        $scope.post = (data) => {
+            handlePost(data);
+        }
+
+
+
+
     };
     app.controller("MainController", MainController);
 
--- AngularJS_with_apis/productServices.js
+++ AngularJS_with_apis/productServices.js
@@ -1,13 +1,23 @@
 (function () {
   var product = function ($http) {
     var getProduts = function () {
-      return $http.get("http://localhost:44331/api/values")
+      return $http.get("https://localhost:44331/api/values")
+      // https://localhost:44331/api/values
         .then(function (response) {
           return response.data;
         });
     };
 
-    return { getProduts: getProduts };
+    var url = 'https://localhost:44331/api/values',config='application/json';
+
+     var postProduts = function (data) {
+      return $http.post(url,JSON.stringify(data) ,config)
+        .then(function (response) {
+          return response.data;
+        });
+    };
+
+    return { getProduts: getProduts  , postProduts: postProduts };
   };
 
   var module = angular.module("ProductViewer");
